64dcd63181b299e10040aba5732a289f7cd03b4d
radudraghiapd <111497046+radudraghiapd@users.noreply.github.com>
2023-09-16 00:23:13 +0200

    Update Windows_Update_Notification_Script.ps1

--- Windows_Update_Notification_Script.ps1
+++ Windows_Update_Notification_Script.ps1
@@ -1,8 +1,3 @@
-# Define the path for the PowerShell script to check for updates and display notifications
-$scriptPath = "C:\ProgramData\Windows_Update_Notification_Script.ps1"
-
-# Create the PowerShell script to check for updates and display notifications
-$scriptContent = @"
 # Function to check for Windows updates
 Function CheckForUpdates {
     $updateSession = New-Object -ComObject Microsoft.Update.Session
@@ -23,10 +18,6 @@ Function DisplayNotification {
 
     If ($result -eq [System.Windows.MessageBoxResult]::Yes) {
         Invoke-Expression -Command "control /name Microsoft.WindowsUpdate"
-
-        # Add your additional script or command here
-        # For example, you can call another PowerShell script:
-        Invoke-Expression -Command "C:\Path\To\Your\AdditionalScript.ps1"
     }
 }
 
@@ -34,13 +25,3 @@ Function DisplayNotification {
 If (CheckForUpdates) {
     DisplayNotification
 }
-
-"@
-Set-Content -Path $scriptPath -Value $scriptContent
-
-# Register a scheduled task to run the script daily at 12:00 PM
-$taskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File $scriptPath"
-$taskTrigger = New-ScheduledTaskTrigger -Daily -At "12:00 PM"
-Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName "Windows_Update_Check" -User "NT AUTHORITY\SYSTEM" -Force
-
-Write-Host "Script and scheduled task created and configured."
