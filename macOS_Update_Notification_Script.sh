fb34bbcac9048cf6888ebf44696277b39d0be591
radudraghiapd <111497046+radudraghiapd@users.noreply.github.com>
2023-09-19 15:27:30 +0200

    Update macOS_Update_Notification_Script.sh

--- macOS_Update_Notification_Script.sh
+++ macOS_Update_Notification_Script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Get the currently logged-in username
+current_user=$(stat -f %Su /dev/console)
+
+# Set the path to the user's home directory
+user_home_dir=$(eval echo ~$current_user)
+
+# Set the path to the script files
+applescript_file="$user_home_dir/Library/Scripts/macos_update.applescript"
+bash_script_file="$user_home_dir/Library/Scripts/macos_update_script.sh"
+
 # Create the LaunchDaemon plist file
 cat <<EOL > /Library/LaunchDaemons/com.osx.update_check.plist
 <?xml version="1.0" encoding="UTF-8"?>
@@ -9,7 +19,7 @@ cat <<EOL > /Library/LaunchDaemons/com.osx.update_check.plist
    <key>Label</key>
    <string>com.osx.update_check</string>
    <key>Program</key>
-   <string>/Library/Scripts/macos_update_script.sh</string>
+   <string>$bash_script_file</string>
    <key>RunAtLoad</key>
    <true/>
    <key>StartCalendarInterval</key>
@@ -24,77 +34,21 @@ cat <<EOL > /Library/LaunchDaemons/com.osx.update_check.plist
 EOL
 
 # Create the AppleScript file
-cat <<EOL > /Library/Scripts/macos_update.applescript
--- Function to check for software updates
-on checkForUpdates()
-    set updatesAvailable to do shell script "softwareupdate -l"
-    
-    -- Initialize variables to store update information and determine extraction
-    set versionNumber to ""
-    set extractInfo to false
-    
-    -- Split the lines into a list
-    set linesList to paragraphs of updatesAvailable
-    
-    -- Iterate through the list to find the version number
-    repeat with i from 1 to count linesList
-        set thisLine to item i of linesList
-        if thisLine contains "macOS Ventura" then
-            -- Extract the version number
-            set versionNumber to last word of thisLine
-            exit repeat
-        end if
-    end repeat
-    
-    -- If the version number was found, continue extracting other information
-    if versionNumber is not equal to "" then
-        set extractInfo to true
-    end if
-    
-    -- Initialize a variable to store update information
-    set updateInfo to ""
-    
-    -- Extract and format the relevant update information for all updates
-    repeat with i from i to count linesList
-        set thisLine to item i of linesList
-        if extractInfo then
-            if thisLine is not in {"", " ", tab} and thisLine does not contain "Recommended:" and thisLine does not contain "Action:" then
-                set updateInfo to updateInfo & thisLine & return
-            else
-                exit repeat
-            end if
-        end if
-    end repeat
-    
-    return versionNumber & return & updateInfo
-end checkForUpdates
-
--- Function to display a notification
-on displayNotification()
-    set updateDetails to checkForUpdates()
-    
-    set messageText to "A fully up-to-date device is required to ensure that IT can accurately protect your device."
-    set buttonText to "Click \"Open Updates\" to install them."
-    set dialogText to messageText & return & return & updateDetails & return & buttonText
-    display dialog dialogText buttons {"Open Updates", "Dismiss"} default button "Open Updates" with icon caution
-end displayNotification
-
--- Check for updates and display the notification
-displayNotification()
-
+cat <<EOL > "$applescript_file"
+# ... (Your AppleScript code here)
 EOL
 
 # Create the Bash script
-cat <<EOL > /Library/Scripts/macos_update_script.sh
+cat <<EOL > "$bash_script_file"
 #!/bin/bash
-osascript /Library/Scripts/macos_update.applescript
+osascript "$applescript_file"
 EOL
 
-# Set permissions and ownership
-chmod +x /Library/Scripts/macos_update.applescript
-chmod +x /Library/Scripts/macos_update_script.sh
+# Set permissions and ownership based on the current user
+chown "$current_user" "$applescript_file" "$bash_script_file"
+chmod +x "$applescript_file" "$bash_script_file"
 
 # Load the LaunchDaemon
 launchctl load /Library/LaunchDaemons/com.osx.update_check.plist
 
-echo "Scripts and LaunchDaemon created and configured."
+echo "Scripts and LaunchDaemon created and configured for user: $current_user"
