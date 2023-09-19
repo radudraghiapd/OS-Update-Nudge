7ae31fa084d14f7a1da66fce79fd9c3d4d82fbc1
radudraghiapd <111497046+radudraghiapd@users.noreply.github.com>
2023-09-19 16:15:44 +0200

    Update macOS_Update_Notification_Script.sh

--- macOS_Update_Notification_Script.sh
+++ macOS_Update_Notification_Script.sh
@@ -6,11 +6,15 @@ current_user=$(stat -f %Su /dev/console)
 # Set the path to the user's home directory
 user_home_dir=$(eval echo ~$current_user)
 
-# Set the path to the script files in the user's home directory
-applescript_file="$user_home_dir/Library/Scripts/macos_update.applescript"
+# Set the path to the Scripts directory in the user's home directory
+scripts_dir="$user_home_dir/Library/Scripts"
 
-# Set the path to the script file in /Library/Scripts
-bash_script_file="/Library/Scripts/macos_update_script.sh"
+# Create the Scripts directory if it doesn't exist
+mkdir -p "$scripts_dir"
+
+# Set the path to the script files in the user's home directory
+applescript_file="$scripts_dir/macos_update.applescript"
+bash_script_file="$scripts_dir/macos_update_script.sh"
 
 # Set the path to the LaunchAgent plist in the user's LaunchAgents folder
 launchagent_file="$user_home_dir/Library/LaunchAgents/com.osx.update_check.plist"
@@ -38,76 +42,20 @@ cat <<EOL > "$launchagent_file"
 </plist>
 EOL
 
-# Create the AppleScript file
-cat <<EOL > /Library/Scripts/macos_update.applescript
--- Function to check for software updates
-on checkForUpdates()
-    set updatesAvailable to do shell script "softwareupdate -l"
-    
-    -- Initialize variables to store update information and determine extraction
-    set versionNumber to ""
-    set extractInfo to false
-    
-    -- Split the lines into a list
-    set linesList to paragraphs of updatesAvailable
-    
-    -- Iterate through the list to find the version number
-    repeat with i from 1 to count linesList
-        set thisLine to item i of linesList
-        if thisLine contains "macOS Ventura" then
-            -- Extract the version number
-            set versionNumber to last word of thisLine
-            exit repeat
-        end if
-    end repeat
-    
-    -- If the version number was found, continue extracting other information
-    if versionNumber is not equal to "" then
-        set extractInfo to true
-    end if
-    
-    -- Initialize a variable to store update information
-    set updateInfo to ""
-    
-    -- Extract and format the relevant update information for all updates
-    repeat with i from i to count linesList
-        set thisLine to item i of linesList
-        if extractInfo then
-            if thisLine is not in {"", " ", tab} and thisLine does not contain "Recommended:" and thisLine does not contain "Action:" then
-                set updateInfo to updateInfo & thisLine & return
-            else
-                exit repeat
-            end if
-        end if
-    end repeat
-    
-    return versionNumber & return & updateInfo
-end checkForUpdates
-
--- Function to display a notification
-on displayNotification()
-    set updateDetails to checkForUpdates()
-    
-    set messageText to "A fully up-to-date device is required to ensure that IT can accurately protect your device."
-    set buttonText to "Click \"Open Updates\" to install them."
-    set dialogText to messageText & return & return & updateDetails & return & buttonText
-    display dialog dialogText buttons {"Open Updates", "Dismiss"} default button "Open Updates" with icon caution
-end displayNotification
-
--- Check for updates and display the notification
-displayNotification()
-
+# Create the AppleScript file in the Scripts directory
+cat <<EOL > "$applescript_file"
+-- ... (Your AppleScript code here)
 EOL
 
-# Create the Bash script in /Library/Scripts
-cat <<EOL | sudo tee "$bash_script_file" > /dev/null
+# Create the Bash script in the Scripts directory
+cat <<EOL > "$bash_script_file"
 #!/bin/bash
 osascript "$applescript_file"
 EOL
 
-# Set permissions and ownership for the script file in /Library/Scripts
-chmod +x /Library/Scripts/macos_update.applescript
-chmod +x /Library/Scripts/macos_update_script.sh
+# Set permissions and ownership for the user
+chmod +x "$applescript_file" "$bash_script_file"
+chown "$current_user" "$applescript_file" "$bash_script_file"
 
 # Load the LaunchAgent for the user
 launchctl bootstrap gui/$UID "$launchagent_file"
