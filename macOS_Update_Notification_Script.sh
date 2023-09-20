ed3e80bbe0394a2aaa7647a8747edf787d04db3d
radudraghiapd <111497046+radudraghiapd@users.noreply.github.com>
2023-09-20 22:57:11 +0200

    Update macOS_Update_Notification_Script.sh

--- macOS_Update_Notification_Script.sh
+++ macOS_Update_Notification_Script.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
+# Function to check for software updates
+checkForUpdates() {
+    updatesAvailable=$(softwareupdate -l)
+
+    # Initialize variables to store update information and determine extraction
+    extractInfo=false
+
+    # Split the lines into a list
+    IFS=$'\n' read -r -a linesList <<< "$updatesAvailable"
+
+    # Initialize a variable to store update information
+    updateInfo=""
+
+    # Extract and format the relevant update information for all updates
+    for thisLine in "${linesList[@]}"; do
+        if $extractInfo && [[ $thisLine != "" && $thisLine != " " && $thisLine != *$'\t'* && $thisLine != *"Recommended:"* && $thisLine != *"Action:"* ]]; then
+            updateInfo+="$thisLine"$'\n'
+        else
+            break
+        fi
+    done
+
+    if [[ -n "$updateInfo" ]]; then
+        echo "$updateInfo"
+    fi
+}
+
 # Function to display a notification
 displayNotification() {
-    # Function to check for software updates
-    checkForUpdates() {
-        updatesAvailable=$(softwareupdate -l)
-
-        # Initialize variables to store update information and determine extraction
-        extractInfo=false
-
-        # Split the lines into a list
-        IFS=$'\n' read -r -a linesList <<< "$updatesAvailable"
-
-        # Initialize a variable to store update information
-        updateInfo=""
-
-        # Extract and format the relevant update information for all updates
-        for thisLine in "${linesList[@]}"; do
-            if $extractInfo && [[ $thisLine != "" && $thisLine != " " && $thisLine != *$'\t'* && $thisLine != *"Recommended:"* && $thisLine != *"Action:"* ]]; then
-                updateInfo+="$thisLine"$'\n'
-            else
-                break
-            fi
-        done
-
-        if [[ -n "$updateInfo" ]]; then
-            echo "$updateInfo"
-        fi
-    }
-
     updateDetails=$(checkForUpdates)
 
     if [[ -n "$updateDetails" ]]; then
@@ -57,38 +57,31 @@ if [ ! -f "$applescript_file" ]; then
     cat <<EOL > "$applescript_file"
 -- Function to display a notification
 on displayNotification()
-    -- Function to check for software updates
-    on checkForUpdates()
-        set updatesAvailable to do shell script "softwareupdate -l"
-        
-        -- Initialize variables to store update information and determine extraction
-        set extractInfo to false
-        
-        -- Split the lines into a list
-        set linesList to paragraphs of updatesAvailable
-        
-        -- Initialize a variable to store update information
-        set updateInfo to ""
-        
-        -- Extract and format the relevant update information for all updates
-        repeat with i from 1 to count linesList
-            set thisLine to item i of linesList
-            if thisLine is not in {"", " ", tab} and thisLine does not contain "Recommended:" and thisLine does not contain "Action:" then
-                set updateInfo to updateInfo & thisLine & return
-            else
-                exit repeat
-            end if
-        end repeat
-        
-        return updateInfo
-    end checkForUpdates
-
-    set updateDetails to checkForUpdates()
-
-    if updateDetails is not equal to "" then
+    set updateDetails to do shell script "/usr/bin/softwareupdate -l"
+    
+    -- Initialize variables to store update information and determine extraction
+    set extractInfo to false
+    
+    -- Split the lines into a list
+    set linesList to paragraphs of updateDetails
+    
+    -- Initialize a variable to store update information
+    set updateInfo to ""
+    
+    -- Extract and format the relevant update information for all updates
+    repeat with i from 1 to count linesList
+        set thisLine to item i of linesList
+        if extractInfo and thisLine is not in {"", " ", tab} and thisLine does not contain "Recommended:" and thisLine does not contain "Action:" then
+            set updateInfo to updateInfo & thisLine & return
+        else
+            exit repeat
+        end if
+    end repeat
+    
+    if updateInfo is not equal to "" then
         set messageText to "A fully up-to-date device is required to ensure that IT can accurately protect your device."
         set buttonText to "Click \"Open Updates\" to install them."
-        set dialogText to messageText & return & return & updateDetails & return & buttonText
+        set dialogText to messageText & return & return & updateInfo & return & buttonText
         display dialog dialogText buttons {"Open Updates", "Dismiss"} default button "Open Updates" with icon caution
     end if
 end displayNotification
