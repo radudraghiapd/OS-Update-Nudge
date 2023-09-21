baf41d2b33b5fdc7442e7f269b258b9906f31195
radudraghiapd <111497046+radudraghiapd@users.noreply.github.com>
2023-09-21 13:32:35 +0200

    Update macOS_Update_Notification_Script.sh

--- macOS_Update_Notification_Script.sh
+++ macOS_Update_Notification_Script.sh
@@ -1,58 +1,78 @@
 #!/bin/bash
 
-# Define the user's home directory
-user_home="$HOME"
+# Directory paths
+launch_agents_dir="/Library/LaunchAgents"
+scripts_dir="/Library/Scripts"
 
-# Define script files and folders
-scripts_folder="$user_home/Library/Scripts"
-applescript_file="$scripts_folder/macos_update.applescript"
-bash_script_file="$scripts_folder/check_updates.sh"
-launch_agent_file="$user_home/Library/LaunchAgents/com.osxupdatecheck.plist"
+# Create Scripts directory if it doesn't exist
+if [ ! -d "$scripts_dir" ]; then
+    sudo mkdir -p "$scripts_dir"
+fi
 
-# Create the "Scripts" folder if it doesn't exist
-mkdir -p "$scripts_folder"
-
-# Create and populate the macos_update.applescript file
-cat > "$applescript_file" <<EOF
--- Function to check for software updates
-on checkForUpdates()
-    do shell script "softwareupdate -l"
-end checkForUpdates
-
--- Function to display a notification
-on displayNotification()
-    set updateDetails to checkForUpdates()
-    set messageText to "A fully up-to-date device is required to ensure that IT can accurately protect your device."
-    set infoText to "Update Details:" & return & updateDetails
-    set buttonText to "Click \"Open Updates\" to install them."
-    display dialog messageText & return & return & infoText & return & buttonText buttons {"Open Updates", "Dismiss"} default button "Open Updates" with icon caution
-end displayNotification
-
--- Check for updates and display the notification
-displayNotification()
-EOF
-
-# Create and populate the check_updates.sh file
-cat > "$bash_script_file" <<EOF
+# Create check_updates.sh script
+cat > "$scripts_dir/check_updates.sh" <<EOF
 #!/bin/bash
-
 # Function to check for updates
 function checkForUpdates() {
     softwareupdate -l
 }
-
 # Check for updates
 update_check=\$(checkForUpdates 2>&1)
-
 # Check if updates are available
 if ! echo "\$update_check" | grep -q "No new software available."; then
     # Updates available, execute the AppleScript to display the dialog
-    osascript "$applescript_file"
+    osascript <<EOF
+    display dialog "A fully up-to-date device is required to ensure that IT can accurately protect your device." & return & return & "Info about the update goes here." & return & return & "Click \"Open Updates\" to install them." buttons {"Open Updates", "Dismiss"} default button "Open Updates" with icon caution
+    
+    set buttonChoice to button returned of result
+    
+    if buttonChoice is equal to "Open Updates" then
+        do shell script "open /System/Library/PreferencePanes/SoftwareUpdate.prefPane"
+    end if
+EOF
 fi
 EOF
 
-# Create and populate the com.osxupdatecheck.plist file
-cat > "$launch_agent_file" <<EOF
+# Make check_updates.sh executable
+sudo chmod +x "$scripts_dir/check_updates.sh"
+
+# Create macos_update.applescript
+cat > "$scripts_dir/macos_update.applescript" <<EOF
+-- Function to check for software updates
+on checkForUpdates()
+    do shell script "softwareupdate -l"
+end checkForUpdates
+-- Check for updates
+set updatesAvailable to checkForUpdates()
+-- Function to display a notification
+on displayNotification()
+    set updateDetails to do shell script "softwareupdate -l"
+    
+    set messageText to "A fully up-to-date device is required to ensure that IT can accurately protect your device."
+    set buttonText to "Click \"Open Updates\" to install them."
+    set dialogText to messageText & return & return & updateDetails & return & buttonText
+    
+    display dialog dialogText buttons {"Open Updates", "Dismiss"} default button "Open Updates" with icon caution
+end displayNotification
+-- Display the notification only if updates are available
+if updatesAvailable contains "No new software available." then
+    -- No updates available, do nothing
+else
+    -- Updates available, display the notification
+    displayNotification()
+    
+    -- Capture the response
+    set response to button returned of result
+    
+    -- Check the response and open the Software Update preference pane if "Open Updates" was clicked
+    if response is equal to "Open Updates" then
+        do shell script "open /System/Library/PreferencePanes/SoftwareUpdate.prefPane"
+    end if
+end if
+EOF
+
+# Create com.osx.updatecheck.plist
+cat > "$launch_agents_dir/com.osxupdatecheck.plist" <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -61,26 +81,24 @@ cat > "$launch_agent_file" <<EOF
     <string>com.osxupdatecheck</string>
     <key>ProgramArguments</key>
     <array>
-        <string>/bin/bash</string>
-        <string>$bash_script_file</string>
+        <string>/Library/Scripts/check_updates.sh</string>
     </array>
+    <key>StartCalendarInterval</key>
+    <dict>
+        <key>Hour</key>
+        <integer>12</integer> <!-- Adjust the hour as needed -->
+        <key>Minute</key>
+        <integer>0</integer>
+    </dict>
     <key>StandardOutPath</key>
     <string>/tmp/runscript.log</string>
     <key>StandardErrorPath</key>
     <string>/tmp/runscript-error.log</string>
-    <key>StartCalendarInterval</key>
-    <dict>
-        <key>Hour</key>
-        <integer>12</integer>
-        <key>Minute</key>
-        <integer>0</integer>
-    </dict>
 </dict>
 </plist>
 EOF
 
-# Make the script files executable
-chmod +x "$applescript_file"
-chmod +x "$bash_script_file"
+# Set permissions
+sudo chmod 644 "$launch_agents_dir/com.osxupdatecheck.plist"
 
-echo "Files and folder created successfully."
+echo "Files and folders created successfully."
